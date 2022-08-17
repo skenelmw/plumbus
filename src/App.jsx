18f68a14123b72d730251e8c70684a2c4eb4682c
skenelmw <skenelmw@gmail.com>
2022-08-17 15:32:21 +1000

    needa learn wouter

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,32 @@ function App() {
     const [searchType, setSearchType] = useState("")
     const [search, setSearch] = useState("")
     const [results, setResults] = useState([])
+    const [resultInfo, setResultInfo] = useState()
 
     const handleSearch = (e) => {
       e.preventDefault();
       fetch("https://rickandmortyapi.com/api/" + searchType + "/?name=" + search).then(res => {
         return res.json();
       })
-      .then(console.log)
+      .then(res => {
+        setResults(res.results)
+        setResultInfo(res.info)
+      })
+
     }
 
     function handleTextChange(e) {
       setSearch(e.target.value);
+    }
+    let resultDisplay;
+    if (searchType == "character" && results.length !== 0){
+      resultDisplay = results.map(results => 
+        <li
+        key={results.id}
+        >
+          {results.name}
+        </li>
+        );
     }
 
     return (
@@ -36,6 +51,7 @@ function App() {
             Submit
           </button>
         </form>
+        {resultDisplay}
       </div>
     )
  }
